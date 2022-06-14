522fed4511fb961c76b60040f4e83f1357da01c1
Dilshan Subhashana Rajapaksha <106365502+subhashana77@users.noreply.github.com>
2022-06-15 00:24:47 +0530

    password encryption

--- api/client/client-register.php
+++ api/client/client-register.php
@@ -49,6 +49,10 @@ if ($requestBody['name'] == null) {
         null
     );
 } else {
+
+    $password = $requestBody['password'];
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
     $checkTelephone = DBUtil::executeQuery(
         $connection,
         "SELECT telephone FROM client WHERE telephone = ?",
@@ -76,11 +80,10 @@ if ($requestBody['name'] == null) {
     } else {
         $result = DBUtil::executeUpdate(
             $connection,
-            "INSERT INTO client (name, telephone, email, password, country, address, reg_date) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            "INSERT INTO client (name, telephone, email, '$hash', country, address, reg_date) VALUES (?, ?, ?, ?, ?, ?, ?)",
             $requestBody['name'],
             $requestBody['telephone'],
             $requestBody['email'],
-            $requestBody['password'],
             $requestBody['country'],
             $requestBody['address'],
             $requestBody['reg_date']
